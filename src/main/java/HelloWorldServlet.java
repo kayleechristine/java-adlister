6186fe67a741fab544a0336a42e1fbc07b5dd1a6
Kaylee Morris <kaylee.christine.morris@gmail.com>
2023-01-26 14:12:21 -0600

    finished the servlet exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,26 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        res.setContentType("text/html");
+        PrintWriter write = res.getWriter();
 
+        //write.println("<h1> Hello World! </h1>");
+        String name = req.getParameter("name");
+
+        if (name != null) {
+            write.println("<h1> Hello " + name + "! </h1>");
+        } else {
+            write.println("<h1> Hello World! </h1>");
+        }
     }
 }
---
+++ src/main/java/ViewCountServlet.java
@@ -0,0 +1,28 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "ViewCountServlet", urlPatterns = "/count")
+
+public class ViewCountServlet extends HttpServlet {
+
+    private int count = 0;
+
+    @Override protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        String reset = req.getParameter("reset");
+
+        if (reset != null) {
+            count = 0;
+        } else {
+            count++;
+        }
+        out.println("<h1>Count: " + count + "</h1>");
+
+    }
+}
